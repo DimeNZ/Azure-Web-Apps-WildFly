c0b6b0fd54e1e4f87b3549167ff3484fb1082876
Zan Kavtaskin <kavtaskinz@msl1492.local>
2016-12-03 14:58:04 +0000

    Simplified launcher

--- wwwroot/virtualapplicationwildfly/azure_wf_launcher.ps1
+++ wwwroot/virtualapplicationwildfly/azure_wf_launcher.ps1
@@ -1,49 +1,22 @@
 $httpPlatformPort = (get-item env:"HTTP_PLATFORM_PORT").Value
 $homePath = (get-item env:"HOME").Value
-$originalWFFolder = "WildFlyGoldCopy"
-
-
-Write-Output "Step 1, clean up old folders"
 
 $siteRootWF = "$homePath\site\wwwroot\virtualapplicationwildfly"
-$oldDirToDeleteFilter = "WF_$($env:COMPUTERNAME)_*"
+$clonedWFFolder = "WF_$($env:COMPUTERNAME)_$(Get-Date -format 'yyyyMMdd_HHmmss')_$($httpPlatformPort)"
 
-Write-Output "Checking for old files that match pattern: $oldDirToDeleteFilter"
-
-$sortedFolders = Get-ChildItem -Path $siteRootWF -Filter $oldDirToDeleteFilter -Directory | sort -Property CreationTime -Descending
-
-if($sortedFolders.Count -gt 0) {
-    Write-Output "Sorted folders: $sortedFolders"
-
-    Write-Output "Deleting all folders that match pattern, apart from $($sortedFolders[0].FullName)"
-
-    for($index = 1; $index -lt $sortedFolders.Length; $index++) {
-        Remove-Item -Path $sortedFolders[$index].FullName -Force -Recurse
-        Write-Output "Removed folder $($sortedFolders[$index].FullName)"
-    }
-} else {
-    Write-Output "There is nothing to delete"
-}
+Write-Output "Step 1, clean up old folders"
+Get-ChildItem -Path $siteRootWF -Filter "WF_$($env:COMPUTERNAME)_*" -Directory | 
+                            sort -Property CreationTime -Descending | 
+                            select -Skip 1 | 
+                            Remove-Item -Force -Recurse -Verbose
 
 
-Write-Output "Step 2, creating new folder and  copy files into it"
-
-$dateAndTimeStamp = Get-Date -format 'yyyyMMdd_HHmmss'
-$clonedWFFolder = "WF_$($env:COMPUTERNAME)_$($dateAndTimeStamp)_$($httpPlatformPort)"
-Write-Output "New WF folder name $clonedWFFolder"
-
-Copy-Item "$siteRootWF\$originalWFFolder" -Destination "$siteRootWF\$clonedWFFolder" -Recurse
+Write-Output "Step 2, creating new folder and copy files into it"
+Copy-Item "$siteRootWF\WildFlyGoldCopy" -Destination "$siteRootWF\$clonedWFFolder" -Recurse -Verbose
 
 
 Write-Output "Step 3, Invoking cloned WildFly"
-$args = $(
+Start-Process -FilePath "$siteRootWF\$clonedWFFolder\bin\standalone.bat" -NoNewWindow -ArgumentList $(
     "-Djboss.http.port=$httpPlatformPort", 
     "-Djboss.server.log.dir=$homePath\LogFiles\$clonedWFFolder"
     )
-
-
-Start-Process -FilePath "$siteRootWF\$clonedWFFolder\bin\standalone.bat" -NoNewWindow -ArgumentList $args 
-
-Write-Output "Exiting Azure_WF_Launcher"
-
-
