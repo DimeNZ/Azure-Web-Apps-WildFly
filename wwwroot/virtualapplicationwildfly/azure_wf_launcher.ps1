e7fe0ef3fc55fda4a258f439da94f16f0c335126
Zan Kavtaskin <zan.kavtaskin@gmail.com>
2017-03-27 23:26:40 +0100

    No need to copy the whole WF

--- wwwroot/virtualapplicationwildfly/azure_wf_launcher.ps1
+++ wwwroot/virtualapplicationwildfly/azure_wf_launcher.ps1
@@ -10,13 +10,18 @@ Get-ChildItem -Path $siteRootWF -Filter "WF_$($env:COMPUTERNAME)_*" -Directory |
                             select -Skip 1 | 
                             Remove-Item -Recurse -Force
 
-
 Write-Output "Step 2, creating new folder and copy files into it"
-Copy-Item "$siteRootWF\WildFlyGoldCopy" -Destination "$siteRootWF\$clonedWFFolder" -Recurse
+Copy-Item "$siteRootWF\WildFlyGoldCopy\standalone" -Destination "$siteRootWF\$clonedWFFolder\standalone" -Recurse
 
 
 Write-Output "Step 3, Invoking cloned WildFly"
-Start-Process -FilePath "$siteRootWF\$clonedWFFolder\bin\standalone.bat" -NoNewWindow -ArgumentList $(
+Start-Process -FilePath "$siteRootWF\WildFlyGoldCopy\bin\standalone.bat" -NoNewWindow -ArgumentList $(
     "-Djboss.http.port=$httpPlatformPort", 
-    "-Djboss.server.log.dir=$homePath\LogFiles\$clonedWFFolder"
+    "-Djboss.server.log.dir=$homePath\LogFiles\$clonedWFFolder",
+    "-Djboss.controller.temp.dir=$siteRootWF\$clonedWFFolder\controller",
+    "-Djboss.server.temp.dir=$siteRootWF\$clonedWFFolder\temp",
+    "-Djboss.server.data.dir=$siteRootWF\$clonedWFFolder\data",
+    "-Djboss.server.deploy.dir=$siteRootWF\$clonedWFFolder\deploy",
+    "-Djboss.server.base.dir=$siteRootWF\$clonedWFFolder\standalone",
+    "-Djboss.home.dir=$siteRootWF\WildFlyGoldCopy\standalone"
     )
